0fdd76f7f9409d87342cfd0abfe12d89948e5684
Faizan <Faizan>
2022-10-15 21:11:08 +0500

    tiktok mern by faizan commit

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    url: "https://v16-webapp.tiktok.com/df5162c74ff1485b9ec3d4df6310a8ff/634959ac/video/tos/useast2a/tos-useast2a-pve-0037c001-aiso/aa68c06b85794ad2b5f7624fd862b2a2/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=3768&bt=1884&cs=0&ds=3&ft=WgGbvN75VQ9wUhN1.1W.CSpxatk3hdxwi88Yprl8eC_O&mime_type=video_mp4&qs=0&rc=PGk2NTQ5Nzk2OjtlaWU0PEBpM3lzcTM6ZnJzZjMzZjczM0BfMjFfNi1jXmIxYmNeLWM1YSNnazRwcjRnM3NgLS1kMWNzcw%3D%3D&l=20221014064411010245099002090A1A6E&btag=80000",
+    channel: "Faizan Channel",
+    description: "something reallly cool in the form of dairy",
+    song: "mitti day tibbay tu sajjay pasy",
+    likes: "102",
+    comments: "200",
+    shares: "30",
+  },
+  {
+    url: "https://v16-webapp.tiktok.com/8ad99d8913edc206bd82a1aadf35aa58/63495b92/video/tos/useast2a/tos-useast2a-pve-0037c001-aiso/a90f9d5a11cf47eb8a433b8385bc5ed2/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=4314&bt=2157&cs=0&ds=3&ft=kLO5qyFUZmo0P9QBVBkVQwZWkiHKJdmC0&mime_type=video_mp4&qs=0&rc=ZGg2aDtnNWc5OWk2NTY4N0Bpamo3ajU6Zjw2ZTMzZjczM0AtNi1hLl4xNTExLjVgNTYyYSNscmlxcjRnM2dgLS1kMWNzcw%3D%3D&l=20221014065224010244089071240AD1A1&btag=80000",
+    channel: "Faizan Channel",
+    description: "something reallly cool in the form of dairy",
+    song: "mitti day tibbay tu sajjay pasy",
+    likes: "102",
+    comments: "200",
+    shares: "30",
+  },
+];
+
+export default data;
---
+++ server.js
@@ -0,0 +1,51 @@
+import express, { application } from "express";
+import mongoose from "mongoose";
+import Data from "./data.js";
+import Videos from "./dbModel.js";
+// App Config
+const app = express();
+const port = process.env.PORT || 9000;
+const dbConnect =
+  "mongodb+srv://faizan-admin:ArfanBhai12@cluster0.imwx1tt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+// middlewares
+app.use(express.json());
+app.use();
+// if you will not add this then you will get the error of not adding the JSON into the Database
+mongoose.connect(
+  dbConnect,
+  { useNewUrlParser: true },
+  { useUnifiedToplogy: true }
+);
+// DB Config
+
+// API Endpoints
+app.get("/", (req, res) => res.status(200).send("Hello World Baby!"));
+
+app.get("/v1/posts", (req, res) => {
+  res.status(200).send(Data);
+});
+app.get("/v2/posts", (req, res) => {
+  Videos.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+app.post("/v2/posts", (req, res) => {
+  // POST request is used to ADD DATA to the database
+  // It will let us add the video data/Document to tiktokVideos Collection
+  const dbVideos = req.body;
+
+  Videos.create(dbVideos, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+// Listen
+app.listen(port, () => console.log(`listening on localhost ${port}`));
